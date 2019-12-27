3531cd329b923758ee06ecc65def60bfb6e140c5
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2019-12-27 21:29:13 +0700

    remove the template index

---
+++ components/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h>Hello world!</h>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
---
+++ pages/landing.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-content>
+            <home></home>
+        </v-content>
+    </div>
+</template>
+
+<script>
+import Home from '@/components/home'
+
+export default {
+    name: 'landing',
+    components:{
+        Home
+    }
+};
+</script>
